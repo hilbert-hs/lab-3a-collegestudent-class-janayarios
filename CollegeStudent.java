9b9d23f2a9bfb18b6572194dd269dd420df5adb6
janayarios <jesus_anaya57@icloud.com>
2025-12-12 09:54:26 +0000

    finish

--- CollegeStudent.java
+++ CollegeStudent.java
@@ -1,2 +1,87 @@
 // Create your CollegeStudent class here
+public class CollegeStudent {
 
+
+    
+    private String name;
+    private String courseTitle;
+    private String courseNumber;
+    private int credits;
+    private int courseFee;
+    private static int nextStudentID = 1;
+    private int costPerCredit = 125;
+    private int studentID;
+
+    public CollegeStudent() {
+        studentID = nextStudentID;
+        nextStudentID++;
+    }
+
+    public CollegeStudent(String _name) {
+        name = _name;
+        studentID = nextStudentID;
+        nextStudentID++;
+    }
+
+    public CollegeStudent(String _name, String _courseTitle, String _courseNumber, int _credits) {
+        name = _name;
+        courseTitle = _courseTitle;
+        courseNumber = _courseNumber;
+        credits = _credits;
+        courseFee = costPerCredit * credits;
+        studentID = nextStudentID;
+        nextStudentID++;
+    }
+
+    public void setName(String _name) {
+        name = _name;
+    }
+
+    public void setCourseTitle(String _courseTitle) {
+        courseTitle = _courseTitle;
+    }
+
+    public void setCourseNumber(String _courseNumber) {
+        courseNumber = _courseNumber;
+    }
+
+    public void setCredits(int _credits) {
+        credits = _credits;
+        courseFee = costPerCredit * credits;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public int getCourseFee() {
+        return courseFee;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void printAttributes() {
+        System.out.println("Name: " + name);
+        System.out.println("Course Title: " + courseTitle);
+        System.out.println("Course Number: " + courseNumber);
+        System.out.println("Credits: " + credits);
+        System.out.println("Course Fee: " + courseFee);
+    }
+
+
+}
--- Main.java
+++ Main.java
@@ -5,14 +5,51 @@ class Main {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
+
+
+    System.out.println("Would you like to create a new student? [y/n]: ");
+    String userInput = scan.nextLine();
+    // write a while loop to create and display CollegeStudnets using the below method;
     
-    // write a while loop to create and display CollegeStudnets using the below method
-    
+    while ( (userInput.toLowerCase().equals("y")) ) {
+
+      createStudent(scan);
+
+      System.out.println("Would you like to create a new student? [y/n]: ");
+      userInput = scan.nextLine();
+
+    }
+
   }
 
   private static CollegeStudent createStudent(Scanner scan) {
     // complete this method to create a CollegeStudent object with details provided by the user
-    
+  
+    System.out.println("Enter student name: ");
+    String user_studentName = scan.nextLine();
+
+    System.out.println("Enter course title: ");
+    String user_courseTitle = scan.nextLine();
+
+    System.out.println("Enter course number: ");
+    String user_courseNumber = scan.nextLine();
+
+    System.out.println("Enter student credits: ");
+    int user_studentCredits = scan.nextInt();
+    scan.nextLine(); // allows the while loop in "main" to work
+
+    System.out.println("");
+    System.out.println("---Your new student---");
+    System.out.println("Name: " + user_studentName);
+    System.out.println("Course Title: " + user_courseTitle);
+    System.out.println("Course Number: " + user_courseNumber);
+    System.out.println("Student Credits: " + user_studentCredits);
+    System.out.println("----------------------");
+    System.out.println("");
+
+
+    return new CollegeStudent(user_studentName, user_courseTitle, user_courseNumber, user_studentCredits);
+
   }
     
 }
